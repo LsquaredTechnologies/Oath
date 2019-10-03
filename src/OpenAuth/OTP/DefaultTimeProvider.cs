4fae221ddadac122ffd76e02b0b877d9ee6de4e4
LionelL <lionel.lalande+github@lsquared.fr>
2019-10-03 19:54:10 +0200

    Mark class sealed

--- src/OpenAuth/OTP/DefaultTimeProvider.cs
+++ src/OpenAuth/OTP/DefaultTimeProvider.cs
@@ -5,7 +5,7 @@ namespace Lsquared.Extensions.OpenAuth.OTP
     /// <summary>
     /// Represents the default date/time provider which returns the current date/time.
     /// </summary>
-    public class DefaultTimeProvider : ITimeProvider
+    public sealed class DefaultTimeProvider : ITimeProvider
     {
         /// <inheritdoc />
         public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
